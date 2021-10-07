0919e80d59787d57def46d1cd9895c8463819bee
wesley <wfaria@technomar.com.br>
2021-10-07 08:18:11 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Project for setup next js apps.
+
+Configured:
+
+* Eslint (Airbnb)
+* prettier
+
+
+it was strongly inspired by: [This video](https://www.youtube.com/watch?v=1nVUfZg2dSA&t=1888s)
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+    </Head>
+
+    <main>
+      <h1>Hello World!</h1>
+    </main>
+  </div>
+);
+
+export default Home;
